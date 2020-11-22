bbad5f81e92b2c1deba01d5f3c17d3f477f0c88b
ViviVillalobos79 <vvillalobosv@estudiantec.cr>
2020-11-21 21:43:23 -0600

    info reto

--- FRONTEND/src/app/pages/ver-competencia-info/ver-competencia-info.component.ts
+++ FRONTEND/src/app/pages/ver-competencia-info/ver-competencia-info.component.ts
@@ -1,62 +1,104 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Carrera } from 'src/app/models/Carreras';
+import { CarrerasService } from '../../services/Carreras/carreras.service';
 
 @Component({
   selector: 'app-ver-competencia-info',
   templateUrl: './ver-competencia-info.component.html',
-  styleUrls: ['./ver-competencia-info.component.css']
+  styleUrls: ['./ver-competencia-info.component.css'],
+  providers: [CarrerasService],
 })
 export class VerCompetenciaInfoComponent implements OnInit {
-
   informacion = {
     Nombre: 'Milla',
     Descripcion: 'Debe hacer una milla para ganar el reto',
-    Categorias : ['Junior','Sub-23','Open','Elite','Master A','Master B','Master C']
+    Categorias: [
+      'Junior',
+      'Sub-23',
+      'Open',
+      'Elite',
+      'Master A',
+      'Master B',
+      'Master C',
+    ],
   };
 
+  informacionA = {
+    Nombre: 'Milla',
+    Descripcion: 'Debe hacer una milla para ganar el reto',
+    Categorias: [
+      'Junior',
+      'Sub-23',
+      'Open',
+      'Elite',
+      'Master A',
+      'Master B',
+      'Master C',
+    ],
+  };
+
+  carrera:Carrera = new Carrera();
+
   top = [
-    {Nombre : 'Viviana', Edad:'12', Tiempo:'2h3min5seg'},
-    {Nombre : 'Luis', Edad:'12', Tiempo:'2h3min5seg'},
-    {Nombre : 'Fabian', Edad:'12', Tiempo:'2h3min5seg'},
-    {Nombre : 'Samuel', Edad:'12', Tiempo:'2h3min5seg'},
-    {Nombre : 'Lucia', Edad:'12', Tiempo:'2h3min5seg'},
-    {Nombre : 'Natalia', Edad:'12', Tiempo:'2h3min5seg'},
-    {Nombre : 'Enrique', Edad:'12', Tiempo:'2h3min5seg'},
-    {Nombre : 'Pamela', Edad:'12', Tiempo:'2h3min5seg'},
-    {Nombre : 'Estrella', Edad:'12', Tiempo:'2h3min5seg'},
-    {Nombre : 'Nickolas', Edad:'12', Tiempo:'2h3min5seg'}
+    { Nombre: 'Viviana', Edad: '12', Tiempo: '2h3min5seg' },
+    { Nombre: 'Luis', Edad: '12', Tiempo: '2h3min5seg' },
+    { Nombre: 'Fabian', Edad: '12', Tiempo: '2h3min5seg' },
+    { Nombre: 'Samuel', Edad: '12', Tiempo: '2h3min5seg' },
+    { Nombre: 'Lucia', Edad: '12', Tiempo: '2h3min5seg' },
+    { Nombre: 'Natalia', Edad: '12', Tiempo: '2h3min5seg' },
+    { Nombre: 'Enrique', Edad: '12', Tiempo: '2h3min5seg' },
+    { Nombre: 'Pamela', Edad: '12', Tiempo: '2h3min5seg' },
+    { Nombre: 'Estrella', Edad: '12', Tiempo: '2h3min5seg' },
+    { Nombre: 'Nickolas', Edad: '12', Tiempo: '2h3min5seg' },
   ];
   username: string;
-  id:string;
-
+  id: string;
 
   constructor(
-    private router: Router, private _route: ActivatedRoute
+    private router: Router,
+    private _route: ActivatedRoute,
+    private carrerasSvc: CarrerasService
   ) {
     this.username = this._route.snapshot.paramMap.get('username');
     this.id = this._route.snapshot.paramMap.get('id');
+    this.carrerasSvc.getCarrera(this.id).subscribe(res => {
+      this.carrera = res[0];
+      this.setRetosPublicos(this.carrera)
+    });
     //Obtener información del competencia
-   }
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  setRetosPublicos(retos: Carrera) {
+    this.informacionA = {
+      Nombre: retos.nombre,
+      Descripcion: retos.tipoactividad,
+      Categorias: [
+        'Junior',
+        'Sub-23',
+        'Open',
+        'Elite',
+        'Master A',
+        'Master B',
+        'Master C',
+      ],
+    };
   }
 
   iniciogo() {
-    this.router.navigate(['inicio-deport',this.username]);
+    this.router.navigate(['inicio-deport', this.username]);
   }
   buscargo() {
-    this.router.navigate(['buscar',this.username]);
+    this.router.navigate(['buscar', this.username]);
   }
 
   retosGo() {
-    this.router.navigate(['verRetos',this.username]);
+    this.router.navigate(['verRetos', this.username]);
   }
 
   competenciasGo() {
-    this.router.navigate(['verCompetencias',this.username]);
+    this.router.navigate(['verCompetencias', this.username]);
   }
-
-
-
-
 }
--- FRONTEND/src/app/pages/ver-reto-info/ver-reto-info.component.ts
+++ FRONTEND/src/app/pages/ver-reto-info/ver-reto-info.component.ts
@@ -1,37 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Retos } from 'src/app/models/Retos';
+import { RetosService } from '../../services/Retos/retos.service';
 
 @Component({
   selector: 'app-ver-reto-info',
   templateUrl: './ver-reto-info.component.html',
   styleUrls: ['./ver-reto-info.component.css'],
+  providers: [RetosService],
 })
 export class VerRetoInfoComponent implements OnInit {
-
   informacion = {
     Nombre: 'Milla',
     Descripcion: 'Debe hacer una milla para completar el reto',
     Horas: '10',
     Min: '5',
-    Seg: '0'
+    Seg: '0',
   };
 
-  patrocinadores=[];
+  informacionA = {
+    Nombre: 'retos.nombre',
+    Descripcion: '',
+    Periodo: 'retos.periodo'
+  };
+  patrocinadores = [];
 
-  source1:string = '';
-  source2:string = '';
+  source1: string = '';
+  source2: string = '';
   username: string;
-  id:string;
+  id: string;
+  reto:Retos = new Retos();
 
   progresValue: number;
 
   constructor(
-    private router: Router, private _route: ActivatedRoute,
+    private router: Router,
+    private _route: ActivatedRoute,
+    private retosSvc: RetosService
   ) {
     this.username = this._route.snapshot.paramMap.get('username');
     this.id = this._route.snapshot.paramMap.get('id');
     //Obtener información del reto
-   }
+    this.retosSvc.getReto(this.id).subscribe(res => {
+      this.reto = res[0];
+      this.setRetosPublicos(this.reto)
+    });
+  }
 
   ngOnInit(): void {
     this.progresValue = 30;
@@ -41,25 +55,30 @@ export class VerRetoInfoComponent implements OnInit {
     this.patrocinadores.push(this.source2);
   }
 
+  setRetosPublicos(retos: Retos) {
+    this.informacionA = {
+      Nombre: retos.nombre,
+      Descripcion: retos.tipoActividad + ', ' + retos.tipoReto,
+      Periodo: retos.periodo
+    };
+  }
+
   iniciogo() {
-    this.router.navigate(['inicio-deport',this.username]);
+    this.router.navigate(['inicio-deport', this.username]);
   }
   buscargo() {
-    this.router.navigate(['buscar',this.username]);
+    this.router.navigate(['buscar', this.username]);
   }
 
   retosGo() {
-    this.router.navigate(['verRetos',this.username]);
+    this.router.navigate(['verRetos', this.username]);
   }
 
   competenciasGo() {
-    this.router.navigate(['verCompetencias',this.username]);
+    this.router.navigate(['verCompetencias', this.username]);
   }
 
-  actividadGo(){
-    this.router.navigate(['actividad',this.username]);
+  actividadGo() {
+    this.router.navigate(['actividad', this.username]);
   }
-
-
-
 }
